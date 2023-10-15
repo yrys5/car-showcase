00ca10654923cf6db77008b69ed560ecbf9c0bbb
mkostecki <kosteckimaciej@outlook.com>
2023-10-15 20:15:51 +0200

    final-layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,24 @@
-import { Footer, Navbar } from '@/components'
-import './globals.css'
-import type { Metadata } from 'next'
+import { Footer, Navbar } from "@/components";
+import "./globals.css";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Car showcase',
-  description: 'Discover the best cars in the world ',
-}
+  title: "Car showcase",
+  description: "Discover the best cars in the world ",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <Navbar/>
-      <body className="relative">{children}</body>
-      <Footer/>
+      <body className="relative">
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
