8957753e914128e8cb4e0c8418c95968b36acc56
mkostecki <kosteckimaciej@outlook.com>
2023-10-16 18:32:29 +0200

    car-catalogue-final

---
+++ components/SearchBar.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { useState } from "react"
+import { SearchManufacturer } from "."
+
+const SearchBar = () => {
+    const [manufacturer, setManufacturer] = useState("second")
+    const handleSearch = () => {}
+  return (
+    <form className='searchbar' onSubmit={handleSearch}>
+        <div className="searchbar__item">
+            <SearchManufacturer manufacturer={manufacturer} setManufacturer={setManufacturer}/>
+        </div>
+    </form>
+  )
+}
+
+export default SearchBar
