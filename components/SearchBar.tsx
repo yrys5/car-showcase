2b0d6b74e6e61b07667dc228abc2b8e201ce671a
mkostecki <kosteckimaciej@outlook.com>
2023-10-19 21:10:30 +0200

    SSR-and-Custom-Filters-Final

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -42,7 +42,7 @@ const SearchBar = () => {
 
     const newPathname = `${window.location.pathname}?${searchParams.toString()}`
 
-    router.push(newPathname)
+    router.push(newPathname, {scroll: false})
   };
 
   return (
--- types/index.ts
+++ types/index.ts
@@ -5,7 +5,7 @@ export interface CustomButtonProps {
   containerStyles?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
   btnType?: "button" | "submit";
-  textStyles?:  string;
+  textStyles?: string;
   rightIcon?: string;
   isDisabled?: boolean;
 }
@@ -19,7 +19,7 @@ export interface CarProps {
   city_mpg: number;
   class: string;
   combination_mpg: number;
-  cylinders:number;
+  cylinders: number;
   displacement: number;
   drive: string;
   fuel_type: string;
@@ -28,4 +28,22 @@ export interface CarProps {
   model: string;
   transmission: string;
   year: number;
+}
+
+export interface FilterProps {
+  manufacturer: string;
+  year: number;
+  fuel: string;
+  limit: number;
+  model: string;
+}
+
+export interface OptionProps {
+  title: string;
+  value: string;
+}
+
+export interface CustomFilterProps {
+  title: string;
+  options: OptionProps[];
 }
